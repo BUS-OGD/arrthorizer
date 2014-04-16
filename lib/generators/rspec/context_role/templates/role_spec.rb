ae0c82fcb7b431cb7d9a128d8e6cfdaeff7661be
Rene van den Berg <rene.vandenberg@ogd.nl>
2014-04-16 10:15:21 +0200

    Improve generated specs
    * use the apply_to_user matcher
    * build contexts inline so that developers do not need to deal with it at all

--- lib/generators/rspec/context_role/templates/role_spec.rb
+++ lib/generators/rspec/context_role/templates/role_spec.rb
@@ -5,25 +5,25 @@ describe <%= class_name %> do
 
   let(:user) { double(:user) }
 
-  let(:context_hash) { { } }
-  let(:current_context) { Arrthorizer::Context.new(context_hash) }
+  let(:current_context) { { } }
 
   describe :applies_to_user? do
     context "when some_condition" do
       before :each do
-        # TODO: Add the required elements to the context_hash to make the ContextRole apply to the user
+        # TODO: Add the required elements to the current_context
+        # to make the ContextRole apply to the user
       end
 
       it "returns true" do
         pending
 
-        expect(role.applies_to_user?(user, current_context)).to be_true
+        expect(role).to apply_to_user(user).with_context(current_context)
       end
 
       # This is an extremely important test - it safeguards against
       # persisting data between requests.
       specify "no state is maintained in the role object" do
-        role.applies_to_user?(user, current_context)
+        role.applies_to_user?(user, Arrthorizer::Context(current_context))
 
         expect(role.instance.instance_variables).to be_empty
       end
@@ -31,20 +31,20 @@ describe <%= class_name %> do
 
     context "when some_other_condition" do
       before :each do
-        # TODO: Add the required elements to the context_hash
+        # TODO: Add the required elements to the current_context
         # to make the ContextRole *not* apply to the user
       end
 
       it "returns false" do
         pending
 
-        expect(role.applies_to_user?(user, current_context)).to be_false
+        expect(role).not_to apply_to_user(user).with_context(current_context)
       end
 
       # This is an extremely important test - it safeguards against
       # persisting data between requests.
       specify "no state is maintained in the role object" do
-        role.applies_to_user?(user, current_context)
+        role.applies_to_user?(user, Arrthorizer::Context(current_context))
 
         expect(role.instance.instance_variables).to be_empty
       end
